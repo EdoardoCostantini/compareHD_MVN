535097527b8a205f20741cf51190a9d5110281a9
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2021-08-03 18:41:22 +0200

    results: trial results and first steps in analysis

--- code/results_analysis.R
+++ code/results_analysis.R
@@ -2,7 +2,7 @@
 # Objective: Script to plot the results of the simulation
 # Author:    Edoardo Costantini
 # Created:   2021-07-30
-# Modified:  2021-07-30
+# Modified:  2021-08-03
 
 # Load Results ----------------------------------------------------------
 
@@ -22,8 +22,8 @@
   result <- unique(gg_shape$variable)[2] # what result to plot
   met_cond <- paste0(unique(gg_shape$method),
                      collapse = "|")
-  mar_cond <- unique(gg_shape$mar_pred)
-  lam_cond <- unique(gg_shape$lambda)
+  y_cond <- unique(gg_shape$pm)
+  x_cond <- unique(gg_shape$p)
 
   plot1 <- gg_shape %>%
     # Subset
@@ -39,10 +39,10 @@
     geom_boxplot() +
 
     # Grid
-    facet_grid(rows = vars(factor(mar_pred,
-                                  labels = paste0(mar_cond, " as MAR predictor"))),
-               cols = vars(factor(lambda,
-                                  labels = paste0("fl = ", lam_cond)))) +
+    facet_grid(rows = vars(factor(p,
+                                  labels = paste0("pm = ", y_cond))),
+               cols = vars(factor(pm,
+                                  labels = paste0("p = ", x_cond)))) +
     # Format
     theme(text = element_text(size = 15),
           plot.title = element_text(hjust = 0.5),
@@ -54,33 +54,3 @@
          y     = NULL)
 
   plot1
-
-  # FMI and RIV
-  result <- unique(gg_shape$variable)[5] # what result to plot
-
-  plot_2 <- gg_shape %>%
-    # Subset
-    filter(grepl(parm, par)) %>%
-    filter(grepl(met_cond, method)) %>%
-    filter(grepl(result, variable)) %>%
-
-    # Main Plot
-    ggplot(aes(x = variable,
-               y = value,
-               group = mar_pred,
-               fill = mar_pred)) +
-    geom_boxplot() +
-
-    # Grid
-    facet_grid(cols = vars(factor(lambda,
-                                  labels = paste0("fl = ", lam_cond)))) +
-    # Format
-    theme(text = element_text(size = 15),
-          plot.title = element_text(hjust = 0.5),
-          axis.text = element_text(size = 10),
-          axis.text.x = element_blank(),
-          axis.title = element_text(size = 15)) +
-    labs(title = paste0(result, " for ", parm),
-         x     = NULL,
-         y     = NULL)
-  plot_2
--- code/results_pool.R
+++ code/results_pool.R
@@ -2,7 +2,7 @@
 # Objective: Pool the results of a simulation study given its id folder
 # Author:    Edoardo Costantini
 # Created:   2021-07-30
-# Modified:  2021-07-30
+# Modified:  2021-08-03
 
 ## Make sure we have a clean environment:
 rm(list = ls())
@@ -22,3 +22,24 @@ source("./init.R")
 
   out <- do.call(rbind, output$out)
   gg_shape <- reshape2::melt(out, id.var = colnames(out)[1:6])
+
+# Add reference value and coverage
+
+  true_vec <- out %>%
+    # Subset
+    filter(grepl("OG", method)) %>%
+    group_by(p, pm, par) %>%
+    summarize(ref = mean(est))
+
+  out_augmented <- merge(out, true_vec, by = c("pm", "p", "par"))
+  out_augmented_sc <- out_augmented[, c(colnames(out), "ref")]
+  out_augmented_sc$par <- factor(out_augmented_sc$par,
+                                 levels = unique(out$par))
+  output_sr <- out_augmented_sc %>% arrange(rp, p, pm, method, par)
+
+# Add coverage
+  output_sr <- output_sr %>%
+    mutate(CIC = ci.lower < ref & ref < ci.upper)
+
+# Shape for plots
+  gg_shape <- reshape2::melt(output_sr, id.var = colnames(out)[1:6])
